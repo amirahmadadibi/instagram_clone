814953b849693f5cc57ef29c3bc3fb27e91e0ec6
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-08-05 01:24:35 +0430

    week11 E47 make search screen final

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -9,78 +9,85 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff1C1F2E),
       body: SafeArea(
-        child: GridView.custom(
-          gridDelegate: SliverQuiltedGridDelegate(
-              crossAxisCount: 3,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              repeatPattern: QuiltedGridRepeatPattern.inverted,
-              pattern: [
-                QuiltedGridTile(2, 1),
-                QuiltedGridTile(2, 2),
-                QuiltedGridTile(1, 1),
-                QuiltedGridTile(1, 1),
-                QuiltedGridTile(1, 1),
-              ]),
-          childrenDelegate: SliverChildBuilderDelegate(((context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Image.asset('images/item$index.png'),
-                ),
-              ),
-            );
-          }), childCount: 10),
-        ),
-      ),
+          child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: _getSearchBox(),
+          ),
+          SliverToBoxAdapter(
+            child: _getSotryList(),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverGrid(
+              delegate: SliverChildBuilderDelegate(((context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.asset('images/item$index.png'),
+                    ),
+                  ),
+                );
+              }), childCount: 10),
+              gridDelegate: SliverQuiltedGridDelegate(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  repeatPattern: QuiltedGridRepeatPattern.inverted,
+                  pattern: [
+                    QuiltedGridTile(2, 1),
+                    QuiltedGridTile(2, 2),
+                    QuiltedGridTile(1, 1),
+                    QuiltedGridTile(1, 1),
+                    QuiltedGridTile(1, 1),
+                  ]),
+            ),
+          )
+        ],
+      )),
     );
   }
 
-  Widget _getContetn() {
-    return Column(
-      children: [
-        Container(
-          height: 46,
-          margin: EdgeInsets.only(left: 18, right: 18, top: 12),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(13)),
-            color: Color(0xff272B40),
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              children: [
-                Image.asset('images/icon_search.png'),
-                SizedBox(
-                  width: 15,
-                ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search User',
-                      hintStyle: TextStyle(color: Colors.white),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Image.asset('images/icon_scan.png'),
-              ],
+  Widget _getSearchBox() {
+    return Container(
+      height: 46,
+      margin: EdgeInsets.only(left: 18, right: 18, top: 12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(13)),
+        color: Color(0xff272B40),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            Image.asset('images/icon_search.png'),
+            SizedBox(
+              width: 15,
             ),
-          ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search User',
+                  hintStyle: TextStyle(color: Colors.white),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Image.asset('images/icon_scan.png'),
+          ],
         ),
-        _getSotryList()
-      ],
+      ),
     );
   }
 
