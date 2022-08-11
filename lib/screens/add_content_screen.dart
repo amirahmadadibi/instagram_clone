339d519394ac109f04455dfb933b178ce21027c7
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-08-12 02:11:33 +0430

    week11 E51 make GridSectionAndWholScrollingPart

--- lib/screens/add_content_screen.dart
+++ lib/screens/add_content_screen.dart
@@ -8,8 +8,44 @@ class AddContentScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff1C1F2E),
       body: SafeArea(
-        child: Column(
-          children: [],
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: _getHeaderSection(),
+            ),
+            SliverToBoxAdapter(
+              child: _getSelectedImageContainer(),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+              sliver: SliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: FittedBox(
+                          fit: BoxFit.cover,
+                          child: Image.asset('images/item$index.png'),
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: 10,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
