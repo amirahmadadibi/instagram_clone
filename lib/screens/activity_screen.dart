600e70bc8a1ae7c11847d2dec20e286a13338b9d
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-08-13 00:43:41 +0430

    week11 E54 tabbarview

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -22,27 +22,62 @@ class _ActivityScreenState extends State<ActivityScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff1C1F2E),
       body: SafeArea(
-        child: Container(
-          color: Color(0xff1C1F2E),
-          height: 70,
-          child: TabBar(
-            controller: _tabController,
-            labelStyle: TextStyle(fontSize: 20, fontFamily: 'GB'),
-            indicatorPadding: EdgeInsets.symmetric(horizontal: 20),
-            indicatorWeight: 4,
-            indicatorColor: Color(0xffF35383),
-            tabs: [
-              Tab(
-                text: 'Following',
+        child: Column(
+          children: [
+            Container(
+              color: Color(0xff1C1F2E),
+              height: 70,
+              child: TabBar(
+                controller: _tabController,
+                labelStyle: TextStyle(fontSize: 20, fontFamily: 'GB'),
+                indicatorPadding: EdgeInsets.symmetric(horizontal: 20),
+                indicatorWeight: 4,
+                indicatorColor: Color(0xffF35383),
+                tabs: [
+                  Tab(
+                    text: 'Following',
+                  ),
+                  Tab(
+                    text: 'You',
+                  ),
+                ],
               ),
-              Tab(
-                text: 'You',
-              ),
-            ],
-          ),
+            ),
+            Expanded(
+                child: TabBarView(
+              controller: _tabController,
+              children: [
+                CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        return _getRow();
+                      }, childCount: 100),
+                    )
+                  ],
+                ),
+                CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        return Text('AmirahmadAdibi');
+                      }, childCount: 100),
+                    )
+                  ],
+                )
+              ],
+            ))
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _getRow() {
+    return Row(
+      children: [],
     );
   }
 }
